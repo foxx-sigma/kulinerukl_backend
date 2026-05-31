87dc1267d146dd12f75d7d21796c2a0dd383696e
Aesarr <hernandoaesaraulayain@gmail.com>
2026-05-31 18:40:39 +0700

    Script skip ts error di railway

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,12 @@ async function bootstrap() {
   // Security
   app.use(helmet());
   app.use(compression());
-  app.enableCors();
+  app.enableCors({
+  origin: true, // allow semua origin (development)
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
 
   // Global pipes, filters, interceptors
   app.useGlobalPipes(
